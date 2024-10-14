0c0a7857ce08b63920b17fde3c09e6b529ddd879
GODELTECH\o.tikhonova <o.tikhonova@gmail.com>
2024-10-14 13:30:04 +0300

    Task 5

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
+require('dotenv').config();
 const express = require('express');
+const axios = require('axios');
+
 const app = express();
 const PORT = 4000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.get('/meteors', async (req, res) => {
+  try {
+    const nasaToken = process.env.NASA_API_KEY;
+    const apiUrl = process.env.NASA_API_URL;
+    const startDate = process.env.START_DATE;
+    const endDate = process.env.END_DATE;
+
+    const url = `${apiUrl}?start_date=${startDate}&end_date=${endDate}&api_key=${nasaToken}`;
+
+    const response = await axios.get(url);
+
+    res.json(response.data);
+  } catch (error) {
+    console.error('Error fetching data from NASA API:', error);
+    res.status(500).json({ error: 'Error fetching data from NASA API' });
+  }
 });
 
-app.get('/', (req, res) => {
-  res.send('Server is up and running!');
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
