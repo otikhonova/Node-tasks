c07d8687675776e2ab8c136e2fc145a38143b50a
GODELTECH\o.tikhonova <o.tikhonova@gmail.com>
2024-10-14 14:12:30 +0300

    Task-8

--- server.js
+++ server.js
@@ -16,7 +16,18 @@ app.get('/meteors', async (req, res) => {
 
     const response = await axios.get(url);
 
-    res.json(response.data);
+    const filteredData = response.data.near_earth_objects[startDate].map((meteor) => {
+        return {
+          id: meteor.id,
+          name: meteor.name,
+          diameter_meters: meteor.estimated_diameter.meters.estimated_diameter_max,
+          is_potentially_hazardous_asteroid: meteor.is_potentially_hazardous_asteroid,
+          close_approach_date_full: meteor.close_approach_data[0].close_approach_date_full,
+          relative_velocity_kps: meteor.close_approach_data[0].relative_velocity.kilometers_per_second
+        };
+      });
+  
+      res.json(filteredData);
   } catch (error) {
     console.error('Error fetching data from NASA API:', error);
     res.status(500).json({ error: 'Error fetching data from NASA API' });
