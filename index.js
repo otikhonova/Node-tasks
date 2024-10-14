1582a3d5b0b7480a853ea3434bac03532d295aa4
GODELTECH\o.tikhonova <o.tikhonova@gmail.com>
2024-10-14 13:05:16 +0300

    Task-3

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-/*const axios = require('axios');
-const nasaToken = 'NHTI7XjuQc8RrXeYoHTu6PNZ5dh8kvEYh5iQc65Q';
-const startDate = '2024-10-07'; 
-const endDate = '2024-10-11';
+require('dotenv').config();
+const axios = require('axios');
 
-//const url = `https://api.nasa.gov/neo/rest/v1/feed?start_date=${startDate}&end_date=${endDate}&api_key=${nasaToken}`;
-const url = 'https://api.nasa.gov/neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-08&api_key=DEMO_KEY';
+const nasaToken = process.env.NASA_API_KEY;
+const apiUrl = process.env.NASA_API_URL;
+const startDate = process.env.START_DATE;
+const endDate = process.env.END_DATE;
+
+const url = `${apiUrl}?start_date=${startDate}&end_date=${endDate}&api_key=${nasaToken}`;
+
 axios.get(url)
   .then(response => {
     const data = response.data;
@@ -12,40 +15,4 @@ axios.get(url)
   })
   .catch(error => {
     console.error('Error while receiving data:', error);
-  });*/
-const https = require('https');
-
-const apiKey = 'NHTI7XjuQc8RrXeYoHTu6PNZ5dh8kvEYh5iQc65Q';
-
-const startDate = '2024-10-07';
-const endDate = '2024-10-11';  
-
-const options = {
-    hostname: 'api.nasa.gov',
-    path: `/neo/rest/v1/feed?start_date=${startDate}&end_date=${endDate}&api_key=${apiKey}`,
-    method: 'GET',
-    rejectUnauthorized: false
-  };
-
-  const req = https.request(options, (res) => {
-    let data = '';
-  
-    res.on('data', (chunk) => {
-      data += chunk;
-    });
-  
-    res.on('end', () => {
-      try {
-        const jsonData = JSON.parse(data);
-        console.log(`Number of asteroids:`, Object.keys(jsonData.near_earth_objects).length);
-      } catch (error) {
-        console.error('Error while parsing the data:', error);
-      }
-    });
   });
-  
-  req.on('error', (error) => {
-    console.error('Error while running request:', error);
-  });
-  
-  req.end();
