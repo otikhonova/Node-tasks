dd0121a8963d506b5c601ee8d0f6bc0ed757c31a
GODELTECH\o.tikhonova <o.tikhonova@gmail.com>
2024-10-15 14:18:11 +0300

    123

--- errorHandler.js
+++ errorHandler.js
@@ -3,8 +3,7 @@ const Exception = require('./utils/Exception');
 function globalErrorHandler(err, req, res, next) {
   const statusCode = err instanceof Exception ? err.statusCode : 500;
   res.status(statusCode).json({
-    message: err.message,
-    stack: process.env.NODE_ENV === 'production' ? null : err.stack
+    message: err.message
   });
 }
 
