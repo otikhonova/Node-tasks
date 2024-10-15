2213e70e3957ea10ea0648cd1eaec32756fb07d1
GODELTECH\o.tikhonova <o.tikhonova@gmail.com>
2024-10-15 15:43:10 +0300

    Task 10 + fixes

--- delivery/meteorsRoute.js
+++ delivery/meteorsRoute.js
@@ -25,4 +25,25 @@ router.get('/meteors', async (req, res, next) => {
   }
 });
 
+router.get('/meteors/html', async (req, res, next) => {
+  try {
+    let { date, count, 'were-dangerous-meteors': wereDangerousMeteors } = req.query;
+
+    date = date || format(new Date(), 'yyyy-MM-dd');
+
+    count = stringToBoolean(count || 'false');
+    wereDangerousMeteors = stringToBoolean(wereDangerousMeteors || 'false');
+
+    const meteors = await getMeteorsUseCase({ date, count, wereDangerousMeteors });
+
+    if (!meteors || meteors.length === 0) {
+      throw new Exception('No meteors found for the provided date', 404);
+    }
+
+    res.render('meteors.njk', { date, meteors });
+  } catch (error) {
+    next(error); 
+  }
+});
+
 module.exports = router;
