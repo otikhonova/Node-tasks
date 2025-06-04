5fd738e67d7b732e39fb0e441712424e7ca67224
Olga Tikhonova <o.tikhonova@godeltech.com>
2025-06-04 21:14:35 +0300

    Fix validateRequest to properly handle query data

--- middlewares/validateRequest.js
+++ middlewares/validateRequest.js
@@ -1,7 +1,9 @@
 const Joi = require("joi");
 
 const validateRequest = (schema) => (req, res, next) => {
-  const { error } = schema.validate(req.body || req.query);
+  const dataToValidate =
+    req.body && Object.keys(req.body).length > 0 ? req.body : req.query;
+  const { error } = schema.validate(dataToValidate);
   if (error) {
     return res.status(400).json({ message: error.details[0].message });
   }
