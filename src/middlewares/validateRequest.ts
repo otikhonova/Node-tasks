e1622580c6982c1b894220a5e26e8f4e1414976e
Olga Tikhonova <o.tikhonova@godeltech.com>
2025-06-06 12:30:50 +0300

    Fix validateRequest logic, refine meteors data return type, remove debug log

--- src/middlewares/validateRequest.ts
+++ src/middlewares/validateRequest.ts
@@ -1,8 +1,11 @@
 import { Request, Response, NextFunction } from 'express';
 import { Schema } from 'joi';
 
-const validateRequest = (schema: Schema) => (req: Request, res: Response, next: NextFunction): void => {
-  const { error } = schema.validate(req.body || req.query);
+const validateRequest =
+  (schema: Schema) => (req: Request, res: Response, next: NextFunction): void => {
+    const dataToValidate =
+      req.body && Object.keys(req.body).length > 0 ? req.body : req.query;
+    const { error } = schema.validate(dataToValidate);
   
   if (error) {
     res.status(400).json({ message: error.details[0].message });
