552bb3d803a1e2361791fac714358f8765bcf87c
GODELTECH\o.tikhonova <o.tikhonova@gmail.com>
2025-06-06 12:35:47 +0300

    123

--- src/middlewares/validateRequest.ts
+++ src/middlewares/validateRequest.ts
@@ -7,6 +7,7 @@ const validateRequest =
       req.body && Object.keys(req.body).length > 0 ? req.body : req.query;
     const { error } = schema.validate(dataToValidate);
   
+    
   if (error) {
     res.status(400).json({ message: error.details[0].message });
     return;
